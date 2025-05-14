4e4bbf601615a2e9b6a3a4ab2f0b6e299cfcfb59
Fuyumi <44784500+Fuyucch1@users.noreply.github.com>
2025-05-14 22:58:51 +0200

    Go sign

--- app.py
+++ app.py
@@ -321,4 +321,4 @@ if __name__ == '__main__':
     threading.Thread(target=background_refresh, daemon=True).start()
     threading.Thread(target=periodic_save, daemon=True).start()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)

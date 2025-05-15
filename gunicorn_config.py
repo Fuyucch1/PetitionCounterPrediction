96d7f0caf87e1e31417acb8fe65c61164881ffcb
Fuyumi <44784500+Fuyucch1@users.noreply.github.com>
2025-05-15 11:35:17 +0200

    Go sign

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,9 @@
 # gunicorn_config.py
 
 bind = "127.0.0.1:5000"
-workers = 3  # 2-4 is usually fine unless CPU bound
-worker_class = "uvicorn.workers.UvicornWorker"
+workers = 2  # 2-4 is usually fine unless CPU bound
+worker_class = "gthread"
+threads = 4  # number of threads per worker
 timeout = 120
 loglevel = "info"
 

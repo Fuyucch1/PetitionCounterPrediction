8dc0c4b756b82b0d29bad78c024c4f777d8a1b3a
Fuyumi <44784500+Fuyucch1@users.noreply.github.com>
2025-05-16 11:50:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -369,4 +369,4 @@ if __name__ == '__main__':
     werkzeug_logger.addFilter(Filter404())
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
